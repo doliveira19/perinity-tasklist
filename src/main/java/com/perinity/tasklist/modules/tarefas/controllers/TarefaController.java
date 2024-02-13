69624af9e5be114ed34eb773643c28e1d13f8b8a
Daniel Oliveira <dnl.contato@gmail.com>
2024-02-13 14:35:23 -0300

    feat: add get routes for tarefas

--- src/main/java/com/perinity/tasklist/modules/tarefas/controllers/TarefaController.java
+++ src/main/java/com/perinity/tasklist/modules/tarefas/controllers/TarefaController.java
@@ -2,12 +2,17 @@ package com.perinity.tasklist.modules.tarefas.controllers;
 
 import com.perinity.tasklist.modules.tarefas.dto.AllocatePessoaToTarefaDTO;
 import com.perinity.tasklist.modules.tarefas.dto.CreateTarefaDTO;
+import com.perinity.tasklist.modules.tarefas.entites.TarefaEntity;
+import com.perinity.tasklist.modules.tarefas.repositories.TarefaRepository;
 import com.perinity.tasklist.modules.tarefas.useCases.AllocatePessoaToTarefaUseCase;
 import com.perinity.tasklist.modules.tarefas.useCases.CloseTarefaUseCase;
 import com.perinity.tasklist.modules.tarefas.useCases.CreateTarefaUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -21,6 +26,19 @@ public class TarefaController {
 
     @Autowired
     private AllocatePessoaToTarefaUseCase allocatePessoaToTarefaUseCase;
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @GetMapping("/{id}")
+    private Optional<TarefaEntity> getTarefaById(@PathVariable int id) {
+        return this.tarefaRepository.findById(id);
+    }
+
+    @GetMapping
+    private List<TarefaEntity> getAllTarefas() {
+        return this.tarefaRepository.findAll();
+    }
 
     @PostMapping
     private ResponseEntity<Object> createTarefa(@RequestBody CreateTarefaDTO dto) throws Exception {
